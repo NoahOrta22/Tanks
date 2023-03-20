cc0921d67579f4c4209bb51cd5466388b466b6ac
“Josh” <jbrockwilliams@gmail.com>
2023-03-20 13:27:20 -0500

    Power is displayed, Health is displayed

--- main.py
+++ main.py
@@ -54,13 +54,39 @@ right_tank = Tank(screen, 'blue', [WIDTH-110, right_floor_y-40, 70, 40], wall_re
 def bullet_colliding(bullet):
     # making sure bullet is on screen
     if (bullet.x > -1 and bullet.x < WIDTH + 10):
-        # bullet is not colliding with call
+        # bullet is not colliding with wall
         if not (bullet.x >= wall_rect.left-bullet.radius and bullet.x <= wall_rect.right+bullet.radius and bullet.y > wall_rect.top-bullet.radius):
             # bullet is not colliding with the floor
             if (bullet.x <= WIDTH/2 and bullet.y < left_floor_y-bullet.radius) or (bullet.x >= WIDTH/2 and bullet.y < right_floor_y-bullet.radius):
                 return False
     
     return True
+
+#Displays power level of the bullet
+def bulletpower(level,playernum):
+    text = py.font.SysFont("Roboto-Bold", 25).render("Power: " + str(level) + "%", True, 'wheat')
+    if playernum == 1:
+        screen.blit(text, [WIDTH * .75, 0])
+    if playernum == 2:
+        screen.blit(text, [WIDTH * .25, 0])
+
+def health_bars(player1_health, player2_health):
+    if player1_health > 75:
+        player1_health_color = 'green'
+    elif player1_health > 50:
+        player1_health_color = 'yellow'
+    else:
+        player1_health_color = 'red'
+
+    if player2_health > 75:
+        player2_health_color = 'green'
+    elif player2_health > 50:
+        player2_health_color = 'yellow'
+    else:
+        player2_health_color = 'red'
+
+    py.draw.rect(screen, player1_health_color, (1220, 25, player1_health, 25))
+    py.draw.rect(screen, player2_health_color, (20, 25, player2_health, 25))
     
 
 # draw all back screen stuff and tanks
@@ -71,12 +97,17 @@ def draw_stuff():
     draw_left_floor()
     draw_right_floor()
     draw_wall()
+    health_bars(left_tank.health,right_tank.health)
+    
 
 
 left_shoot = False
 start_left_power = False
 
-power = 0
+power_increase = 0
+power_increase2 = 0
+fire_power = 0
+fire_power2 = 0
 angle = 0
 
 
@@ -87,7 +118,6 @@ while run:
     draw_stuff() 
     left_tank.draw_bullet(bullet_colliding(left_tank.bullet))
     right_tank.draw_bullet(bullet_colliding(right_tank.bullet))
-    
 
     # if left_shoot: 
     #     bullet = left_tank.bullet
@@ -111,8 +141,14 @@ while run:
             # doing the power for the left player
             if event.key == py.K_x:
                 left_tank.start_shoot()
+                power_increase2 = 2
+                fire_power2 += power_increase2
+                bulletpower(fire_power2,2)
             if event.key == py.K_SPACE:
                 right_tank.start_shoot()
+                power_increase = 2
+                fire_power += power_increase
+                bulletpower(fire_power,1)
                 # if left_shoot == False: 
                 #     if start_left_power == False:
                 #         start_t = timer.get_ticks()
@@ -130,6 +166,25 @@ while run:
                 #         left_tank.bullet.time = 0
                 #         left_tank.bullet.angle = 1.0
 
+        elif event.type == py.KEYUP:
+             if event.key == py.K_SPACE or event.key == py.K_x:
+                    power_increase = 0
+                    power_increase2 = 0
+    
+    fire_power += power_increase
+    fire_power2 += power_increase2
+
+    if fire_power > 100:
+        fire_power = 100
+    if fire_power2 > 100:
+        fire_power2 =100
+
+    if right_tank.shoot:
+        fire_power=0
+    if left_tank.shoot:
+        fire_power2=0
+    bulletpower(fire_power,1)
+    bulletpower(fire_power2,2)
     right_tank.handle_keys()
     left_tank.handle_keys()
     py.display.flip()
